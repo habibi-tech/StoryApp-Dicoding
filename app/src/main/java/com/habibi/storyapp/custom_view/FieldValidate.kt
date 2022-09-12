cab414cef928fad13e615e7af25cfbafc10f332c
habibitkp <101687909+habibitkp@users.noreply.github.com>
2022-09-12 21:42:16 +0700

    add edittext custom view

---
+++ app/src/main/java/com/habibi/storyapp/custom_view/FieldValidate.kt
@@ -0,0 +1,31 @@
+package com.habibi.storyapp.custom_view
+
+import android.util.Patterns
+import com.habibi.storyapp.R
+
+fun emailValidate(email: String): Int =
+    when {
+        email.isEmpty() -> R.string.error_field_empty
+        !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> R.string.error_field_email_invalid
+        else -> 0
+    }
+
+fun passwordValidate(password: String): Int =
+    when {
+        password.isEmpty() -> R.string.error_field_empty
+        password.length < 6 -> R.string.error_field_password_too_short
+        else -> 0
+    }
+
+fun nameValidate(name: String): Int =
+    when {
+        name.isEmpty() -> R.string.error_field_empty
+        name.length < 3 -> R.string.error_field_name_too_short
+        else -> 0
+    }
+
+fun descriptionValidate(description: String): Int =
+    when {
+        description.isEmpty() -> R.string.error_field_empty
+        else -> 0
+    }
